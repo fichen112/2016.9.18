1aa387ff0f701d63f5aab35c081a60d5b2b277b1
dc0061 <dc0061@xinchenhui.local>
2016-09-18 09:32:12 +0800

    Initial Commit

---
+++ ZYC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYC
+//
+//  Created by dc0061 on 16/9/18.
+//  Copyright © 2016年 dc0061. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZYC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZYC
+//
+//  Created by dc0061 on 16/9/18.
+//  Copyright © 2016年 dc0061. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
